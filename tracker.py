ba054ac119f21d828575920a4f9bf072a35b1921
Sofiia-it <about.sofi.avis@gmail.com>
2026-02-06 16:30:39 +0300

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+print("=== Personal Learning Tracker ===")
+
+while True:
+    print("\n1. Add new learning activity")
+    print("2. View all activities")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        activity = input("What did you learn today? ")
+
+        with open("learning_log.txt", "a", encoding="utf-8") as file:
+            date = datetime.now().strftime("%Y-%m-%d")
+            file.write(f"{date} — {activity}\n")
+
+        print("Saved successfully!")
+
+    elif choice == "2":
+      try:
+         with open("learning_log.txt", "r", encoding="utf-8") as file:
+             print("\nYour learning activities:")
+             for i, line in enumerate(file, start=1):
+                 print(f"{i}. {line.strip()}")
+      except FileNotFoundError:
+          print("No activities yet.")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option. Try again.")
